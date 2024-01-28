f77ac6f1b2d6a7583b78287783b5682317c87cd0
10net-dev <157940934+10net-dev@users.noreply.github.com>
2024-01-28 14:08:52 +0700

    invert

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -6,7 +6,7 @@ export default function Logo() {
       <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
         <defs>
           {/* <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo"> */}
-          <radialGradient cx="13.937%" cy="78.848%" fx="13.937%" fy="78.848%" r="79.941%" id="footer-logo">
+          <radialGradient cx="86.063%" cy="78.848%" fx="86.063%" fy="78.848%" r="79.941%" id="footer-logo">
             <stop stopColor="#6d28d9" offset="0%" />
             <stop stopColor="#c084fc" offset="75.871%" />
             <stop stopColor="#a3e635" offset="100%" />
