5f76d14fff8c5aa3d1d1c41563f4e705fa36022f
10net-dev <157940934+10net-dev@users.noreply.github.com>
2024-01-28 14:12:49 +0700

    122

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -8,8 +8,8 @@ export default function Logo() {
           {/* <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo"> */}
           <radialGradient cx="86.063%" cy="78.848%" fx="86.063%" fy="78.848%" r="79.941%" id="footer-logo">
             <stop stopColor="#a3e635" offset="0%" />
-            <stop stopColor="#c084fc" offset="25.871%" />
-            <stop stopColor="#6d28d9" offset="100%" />
+            <stop stopColor="#c084fc" offset="75.871%" />
+            <stop stopColor="#7e22ce" offset="100%" />
           </radialGradient>
         </defs>
         <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero" />
